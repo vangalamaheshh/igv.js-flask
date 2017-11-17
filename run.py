188f9a021499e38fe67cb79774d826ff997cea00
Mahesh Vangala <vangalamaheshh@gmail.com>
2017-11-17 14:44:26 -0500

    commented out app.run

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from igvjs import app
 
 port = int(os.environ.get('PORT', 5000))
 host = '127.0.0.1' if app.config['DEBUG'] else '0.0.0.0'
-app.run(port=port, host=host)
+#app.run(port=port, host=host)
